418a1afca03fa530c9888ebc62b5b2453ed05c46
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-11 10:34:37 -0600

    console.log answers from inquirer.prompt, including when key pair for engineers and interns

--- app.js
+++ app.js
@@ -4,52 +4,57 @@ const jest = require("jest");
 // prompt user for name, email, id, and school(intern) or github username(engineer and manager)
 function promptUser() {
     return inquirer.prompt([
-        {
+        { // 1
             type: "input",
             name: "name",
             message: "What is your name?"
         },
-        {
-           type:  "input",
-           name: "email",
-           message: "What is your work-related email?"
+        { // 2
+            type: "input",
+            name: "id",
+            message: "What is your employee ID number?"
         },
-        {
+        { // 3
             type: "list",
             name: "role",
             message: "What is your title at your company?",
-            choices: ["manager", "engineer", "intern"]
+            choices: ["Manager", "Engineer", "Intern"]
         },
-        {
+        { // 4
+           type:  "input",
+           name: "email",
+           message: "What is your employee email?"
+        },
+        { // 5
             type: "input",
-            name: "internSchool",
-            message: "Interns, please enter the name of your home university. Others, please enter null",
+            name: "school",
+            message: "Interns, please enter the name of your home university.",
+            when: (data) => data.role === "Intern"
         },
-        {
+        { // 6
             type: "input",
             name: "github",
-            message: "Engineers and managers, please enter your Github username. Interns, enter null."
+            message: "Engineers, please enter your Github username.",
+            when: (data) => data.role === "Engineer"
         }
     ])
-.then(answers => {
+.then(answers => { 
     // store variables with user employee data
     const name = answers.name;
+    const id = answers.id;
     const email = answers.email;
     const role = answers.role;
-    const internSchool = answers.internSchool;
+    const school = answers.school;
     const github = answers.github;
-
-    // console log var checks
+    // console log variable checks
     console.log(name);
+    console.log(id);
     console.log(email);
     console.log(role);
-    console.log(internSchool);
+    console.log(school);
     console.log(github);
 });
 }
-
-
-
-
+// dynamically create html using variables to be written to output/team.html
 // function calls
 promptUser();
