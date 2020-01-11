6ad3222c5a3951e0396ff09b33fd3a3d0f0999bb
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-10 23:57:52 -0600

    passed employee.test.js

--- app.js
+++ app.js
@@ -17,8 +17,39 @@ function promptUser() {
         {
             type: "list",
             name: "role",
-            message: "What is your role at your company?",
+            message: "What is your title at your company?",
             choices: ["manager", "engineer", "intern"]
         },
-    ]) 
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Interns, please enter the name of your home university. Others, please enter null",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Engineers and managers, please enter your Github username. Interns, enter null."
+        }
+    ])
+.then(answers => {
+    // store variables with user employee data
+    const name = answers.name;
+    const email = answers.email;
+    const role = answers.role;
+    const internSchool = answers.internSchool;
+    const github = answers.github;
+
+    // console log var checks
+    console.log(name);
+    console.log(email);
+    console.log(role);
+    console.log(internSchool);
+    console.log(github);
+});
 }
+
+
+
+
+// function calls
+promptUser();
